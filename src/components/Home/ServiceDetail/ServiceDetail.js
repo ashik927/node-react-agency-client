06b9ad7c6da14d60feebe49d627bf03a0b4708c7
ashik927 <ashik1012@gmail.com>
2020-10-17 10:04:41 +0600

    flip add

--- src/components/Home/ServiceDetail/ServiceDetail.js
+++ src/components/Home/ServiceDetail/ServiceDetail.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './ServiceDetail.css'
 
 const ServiceDetail = ({ service }) => {
     return (
-        <div className="col-md-4 text-center">
+        <div className="col-md-4 text-center serviceStyle">
             <img style={{height: '50px'}} src={`data:image/png;base64,${service.image.img}`} alt="" />
             <h5 className="mt-3 mb-3">{service.name}</h5>
             <p className="text-secondary">{service.description}</p>
